09d9adfbe77c07563808fdff0587ab68792c49fd
DMThubhydra <lemdemon@gmail.com>
2023-05-16 16:14:04 +0300

    ку

--- struct1/struct1/struct1.cpp
+++ struct1/struct1/struct1.cpp
@@ -25,7 +25,7 @@ struct Animals
 {
     string name;
     string clas;
-    string alias;
+    string voice;
 };
 void printSmoothing_iron(Smoothing_iron obj)
 {
@@ -45,11 +45,13 @@ void printBoiler(Boiler obj)
     cout << "\tVolume           ::\t" << obj.volume << endl;
     cout << "\tTemperature      ::\t" << obj.temperature << endl;
 }
-void printAnimals(Animals obj)
-{
-    cout << "\tName       ::\t" << obj.name << endl;
-    cout << "\tClass      ::\t" << obj.clas << endl;
-    cout << "\tAlias      ::\t" << obj.alias << endl;
+void printAnimals(const Animal animals[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << "Name: " << animals[i].name << endl;
+        cout << "Class: " << animals[i].clas << endl;
+        cout << "Alias: " << animals[i].voice << endl;
+        cout << endl;
+    }
 }
 void fillSmoothingIron(Smoothing_iron& obj)
 {
@@ -73,10 +75,66 @@ void fillAnimals(Animals& obj)
 {
     cout << "\tName       ::\t" << cin>>obj.name << endl;
     cout << "\tClass      ::\t" << cin>>obj.clas << endl;
-    cout << "\tAlias      ::\t" << cin>>obj.alias << endl;
+    cout << "\tAlias      ::\t" << cin>>obj.voice << endl;
 }
+void makeSound() {
+    cout << "The " << clas << " named " << name << " makes a sound: " << voice << endl;
+}
+
 int main()
 {
-   
+    Animal animals[ANIMALS];
+
+    Animal cat;
+    cat.clas = "Cat";
+    cat.name = "Tom";
+    cat.voice = "Meow";
+    animals[0] = cat;
+
+    Animal dog;
+    dog.name = "Buddy";
+    dog.clas = "Dog";
+    dog.voice = "Woof";
+    animals[1] = dog;
+
+    Animal pig;
+    pig.name = "Porky";
+    pig.clas = "Pig";
+    pig.voice = "Oink";
+    animals[2] = pig;
+
+    Animal horse;
+    horse.name = "Spirit";
+    horse.clas = "Horse";
+    horse.voice = "Neigh";
+    animals[3] = horse;
+
+    printAnimals(animals, ANIMALS);
+
+    int choice;
+    cout << "Select an animal: " << endl;
+    cout << "1. Cat" << endl;
+    cout << "2. Dog" << endl;
+    cout << "3. Pig" << endl;
+    cout << "4. Horse" << endl;
+    cin >> choice;
+
+    switch (choice) {
+    case 1:
+        cat.makeSound();
+        break;
+    case 2:
+        dog.makeSound();
+        break;
+    case 3:
+        pig.makeSound();
+        break;
+    case 4:
+        horse.makeSound();
+        break;
+    default:
+        cout << "Invalid choice!" << endl;
+        break;
+    }    
 }
 
