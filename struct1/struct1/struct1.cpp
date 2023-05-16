94a8073bd75956678a9e715a1aba31087913a821
DMThubhydra <lemdemon@gmail.com>
2023-05-16 19:32:50 +0300

    Main

--- struct1/struct1/struct1.cpp
+++ struct1/struct1/struct1.cpp
@@ -21,11 +21,16 @@ struct Boiler
     int volume;
     float temperature;
 };
+
 struct Animals
 {
     string name;
     string clas;
     string voice;
+
+    void makeSound() {
+        cout << "The " << clas << " named " << name << " makes a sound: " << voice << endl;
+    }
 };
 void printSmoothing_iron(Smoothing_iron obj)
 {
@@ -45,96 +50,88 @@ void printBoiler(Boiler obj)
     cout << "\tVolume           ::\t" << obj.volume << endl;
     cout << "\tTemperature      ::\t" << obj.temperature << endl;
 }
-void printAnimals(const Animal animals[], int size) {
+void printAnimals(const Animals animals[], int size) {
     for (int i = 0; i < size; i++) {
         cout << "Name: " << animals[i].name << endl;
         cout << "Class: " << animals[i].clas << endl;
-        cout << "Alias: " << animals[i].voice << endl;
+        cout << "Voice: " << animals[i].voice << endl;
         cout << endl;
     }
 }
 void fillSmoothingIron(Smoothing_iron& obj)
 {
-    cout << "\tFirma                    ::\t" << getline(cin,obj.firm) << endl;
-    cout << "\tModel                    ::\t" << getline(cin, obj.model) << endl;
-    cout << "\tColor                    ::\t" << getline(cin, obj.color) << endl;
-    cout << "\tMinimal Temperature      ::\t" << cin>>obj.min_temperature << endl;
-    cout << "\tMaximum Temperature      ::\t" << cin>> obj.max_temperature << endl;
-    cout << "\tSupply Steam             ::\t" << cin>> obj.supply_steam << endl;
-    cout << "\tPower                    ::\t" << cin>>obj.power << endl;
+    cout << "\tFirma                    ::\t"; getline(cin, obj.firm);
+    cout << "\tModel                    ::\t"; getline(cin, obj.model);
+    cout << "\tColor                    ::\t"; getline(cin, obj.color);
+    cout << "\tMinimal Temperature      ::\t"; cin >> obj.min_temperature;
+    cout << "\tMaximum Temperature      ::\t"; cin >> obj.max_temperature;
+    cout << "\tSupply Steam             ::\t"; cin >> obj.supply_steam;
+    cout << "\tPower                    ::\t"; cin >> obj.power;
 }
 void fillBoiler(Boiler& obj)
 {
-    cout << "\tFirma          ::\t" << getline(cin, obj.firm) << endl;
-    cout << "\tModel          ::\t" << getline(cin, obj.model) << endl;
-    cout << "\tPower          ::\t" << cin >> obj.power << endl;
-    cout << "\tVolume         ::\t" << cin >> obj.volume << endl;
-    cout << "\tTemperature    ::\t" << cin >> obj.temperature << endl;
-}
-void fillAnimals(Animals& obj)
-{
-    cout << "\tName       ::\t" << cin>>obj.name << endl;
-    cout << "\tClass      ::\t" << cin>>obj.clas << endl;
-    cout << "\tAlias      ::\t" << cin>>obj.voice << endl;
-}
-void makeSound() {
-    cout << "The " << clas << " named " << name << " makes a sound: " << voice << endl;
+    cout << "\tFirma          ::\t"; getline(cin, obj.firm);
+    cout << "\tModel          ::\t"; getline(cin, obj.model);
+    cout << "\tPower          ::\t"; cin >> obj.power;
+    cout << "\tVolume         ::\t"; cin >> obj.volume;
+    cout << "\tTemperature    ::\t"; cin >> obj.temperature;
 }
 
 int main()
 {
-    Animal animals[ANIMALS];
-
-    Animal cat;
-    cat.clas = "Cat";
-    cat.name = "Tom";
-    cat.voice = "Meow";
-    animals[0] = cat;
-
-    Animal dog;
-    dog.name = "Buddy";
-    dog.clas = "Dog";
-    dog.voice = "Woof";
-    animals[1] = dog;
-
-    Animal pig;
-    pig.name = "Porky";
-    pig.clas = "Pig";
-    pig.voice = "Oink";
-    animals[2] = pig;
-
-    Animal horse;
-    horse.name = "Spirit";
-    horse.clas = "Horse";
-    horse.voice = "Neigh";
-    animals[3] = horse;
-
+    Smoothing_iron iron;
+    fillSmoothingIron(iron);
+    printSmoothing_iron(iron);
+    cout << "_______________________________\n";
+    Boiler boiler;
+    fillBoiler(boiler);
+    printBoiler(boiler);
+    cout << "_______________________________\n";
+    const int ANIMALS = 4;
+    Animals animals[ANIMALS] = {
+        {"Cat", "Tom", "Meow"},
+        {"Dog", "Buddy", "Woof"},
+        {"Pig", "Porky", "Oink"},
+        {"Horse", "Spirit", "Neigh"}
+    };
     printAnimals(animals, ANIMALS);
-
     int choice;
-    cout << "Select an animal: " << endl;
-    cout << "1. Cat" << endl;
-    cout << "2. Dog" << endl;
-    cout << "3. Pig" << endl;
-    cout << "4. Horse" << endl;
-    cin >> choice;
+    do {
+        system("cls");
 
-    switch (choice) {
-    case 1:
-        cat.makeSound();
-        break;
-    case 2:
-        dog.makeSound();
-        break;
-    case 3:
-        pig.makeSound();
-        break;
-    case 4:
-        horse.makeSound();
-        break;
-    default:
-        cout << "Invalid choice!" << endl;
-        break;
-    }    
+        cout << "Select an animal: " << endl;
+        cout << "1. Cat" << endl;
+        cout << "2. Dog" << endl;
+        cout << "3. Pig" << endl;
+        cout << "4. Horse" << endl;
+        cout << "0. Exit" << endl;
+        cin >> choice;
+
+
+        switch (choice) {
+        case 1:
+            animals[0].makeSound();
+            break;
+        case 2:
+            animals[1].makeSound();
+            break;
+        case 3:
+            animals[2].makeSound();
+            break;
+        case 4:
+            animals[3].makeSound();
+            break;
+        case 0:
+            cout << "Exiting..." << endl;
+            break;
+        default:
+            cout << "Invalid choice!" << endl;
+            break;
+        }
+
+        cout << endl;
+        system("pause");
+    } while (choice != 0);
 }
+
 
