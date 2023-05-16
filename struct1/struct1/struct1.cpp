77b573c5d36a93e2ee4556c7c244eb07821c15b7
Faust122 <semeniuk_ak22@nuwm.edu.ua>
2023-05-16 13:10:29 +0300

    create 3 struct

--- struct1/struct1/struct1.cpp
+++ struct1/struct1/struct1.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <string>
 
+using namespace std;
+
+struct Smoothing_iron
+{
+    string firm;
+    string model;
+    string color;
+    float min_temperature;
+    float max_temperature;
+    bool supply_steam;
+    float power;
+};
+struct Boiler
+{
+    string firm;
+    string model;
+    float power;
+    int volume;
+    float temperature;
+};
+struct Animals
+{
+    string name;
+    string clas;
+    string alias;
+};
 int main()
 {
-    std::cout << "Hello World!\n";
+    1;
 }
 
